d0eef6ef124efe43cf2a6627e6d4481b5d41ac70
Dzjgz <3457296374@qq.com>
2025-04-19 13:59:11 +0800

    repair

--- env.sh
+++ env.sh
@@ -4,11 +4,3 @@ cd apex
 git checkout 23.08
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ..
-# to avoid lapjv version conflict
-pip uninstall lapjv numpy
-pip install --upgrade numpy
-pip install lapjv --no-cache-dir
-
-# to avoid numba version conflict
-pip uninstall numba llvmlite
-pip install --no-cache-dir numba llvmlite
